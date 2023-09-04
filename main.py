f8d40ce5d402be79e7420c009d29f6854df75cf3
eleahburman <eleahjburman@gmail.com>
2023-09-04 18:11:42 -0400

    Select specific parts of datatable to print

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 connection = sqlite3.connect("gta.db")
 cursor = connection.cursor()
 
-cursor.execute("create table gta (release_year integer, release_name text, city text)")
+cursor.execute("CREATE TABLE IF NOT EXISTS gta (release_year integer, release_name text, city text)")
 release_list = [
   (1997, "Grand Theft Auto", "state of New Guernsey"),
   (1999, "Grand Theft Auto 2", "Anywhere, USA"),
@@ -15,4 +15,14 @@ release_list = [
 ]
 
 cursor.executemany("insert into gta values (?,?,?)", release_list)
+
+#print database rows
+for row in cursor.execute("select * from gta"):
+  print(row)
+
+#print specific rows
+print("**************")
+cursor.execute("select * from gta where city=:c", {"c": "Liberty City"})
+gta_search=cursor.fetchall()
+print(gta_search)
 connection.close()
