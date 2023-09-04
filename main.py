b0c3b9009b8905460423c56152f9529f724f5dc9
eleahburman <eleahjburman@gmail.com>
2023-09-04 18:05:11 -0400

    Create script to dynamically create db

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 
 connection = sqlite3.connect("gta.db")
+cursor = connection.cursor()
+
+cursor.execute("create table gta (release_year integer, release_name text, city text)")
 release_list = [
   (1997, "Grand Theft Auto", "state of New Guernsey"),
   (1999, "Grand Theft Auto 2", "Anywhere, USA"),
@@ -11,4 +14,5 @@ release_list = [
   (2013, "Grand Theft Auto V", "Los Santos")
 ]
 
+cursor.executemany("insert into gta values (?,?,?)", release_list)
 connection.close()
